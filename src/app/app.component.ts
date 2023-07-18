3889ab76763c865abf49b56b152a495e10f26513
Joe Chiao <jchiao@vacc.com.au>
2023-07-18 22:12:22 +1000

    New button to get weather again with new key

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,13 +31,16 @@ export class AppComponent implements OnInit {
       $.each(this.countryData, function(i, item) {
         countrydropdown.append($('<option></option>').val(item.country).html(item.country));
       });
+      localStorage.setItem('keyOrder','key1');
+      let tryagain = $('#tryAgain');
+      tryagain.hide();
   }
 
   populateCity () {
-    
     let citydropdown = $('#cityDropdown');
     citydropdown.show();
     citydropdown.empty();
+
     citydropdown.append('<option disabled selected hidden>Choose City</option>');
     citydropdown.prop('selectedIndex', 0);
 
@@ -49,17 +52,38 @@ export class AppComponent implements OnInit {
   }
 
   getWeather() {
+
+    
     var parameter = "country="+$('#countryDropdown').find(":selected").val() + "&city=" + $('#cityDropdown').find(":selected").val();
+
+    let apiKey = this.rotateApiKey();
     
     $.ajax({
       url: environment.apiEndpoint + "WeatherForecast?" + parameter,
       type: 'GET',
-      headers: { 'Content-Type': 'application/json', 'Accept': 'application/json', 'Access-Control-Allow-Headers': 'Content-Type', 'ApiKey': environment.ApiKey, 'Access-Control-Allow-Origin': '*'},
+      headers: { 'Content-Type': 'application/json', 'Accept': 'application/json', 'Access-Control-Allow-Headers': 'Content-Type', 'ApiKey': apiKey, 'Access-Control-Allow-Origin': '*'},
       dataType: 'json', // added data type
         success:processSuccess,
-        error: handleError
+        error: handleError,
       });
   }
+
+  rotateApiKey() {
+    let keyOrder = localStorage.getItem('keyOrder');
+    if (keyOrder=="key1") 
+      return environment.ApiKey.key1;
+    else if (keyOrder=="key2")
+      return environment.ApiKey.key2;
+    else if (keyOrder=="key3")
+      return environment.ApiKey.key3;
+    else if (keyOrder=="key4")
+      return environment.ApiKey.key4;
+    else if (keyOrder=="key5")
+      return environment.ApiKey.key5;
+    else
+      return "";
+  }
+
  } 
 
  function processSuccess (xhr:any) {
@@ -74,6 +98,8 @@ export class AppComponent implements OnInit {
     //console.log(xhr);
     var description = xhr.weather[0].description == undefined? "no weather report" : xhr.weather[0].description;
     $("#weatherDescription").text($('#cityDropdown').find(":selected").val() + ", " + $('#countryDropdown').find(":selected").val() + " currently has " + description + ".");
+    let tryagain = $('#tryAgain');
+    tryagain.hide();
   }
 }
 
@@ -86,7 +112,27 @@ export class AppComponent implements OnInit {
     } else {
         msg = "[" + xhr.status + "] " + xhr.responseText;
     }
+
+    if (xhr.status === 429) {
+      setKeyOrder();
+      let tryagain = $('#tryAgain');
+      tryagain.show();
+    }
     showSnackBar(msg);
+  }
+
+  function setKeyOrder() {
+    let keyOrder = localStorage.getItem('keyOrder');
+    if (keyOrder=="key1") 
+    localStorage.setItem('keyOrder','key2');
+    else if (keyOrder=="key2")
+    localStorage.setItem('keyOrder','key3');
+    else if (keyOrder=="key3")
+    localStorage.setItem('keyOrder','key4');
+    else if (keyOrder=="key4")
+    localStorage.setItem('keyOrder','key5');
+    else
+    localStorage.setItem('keyOrder','key1');
   }
 
   function showSnackBar(msg:string) {
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,12 @@
 export const environment = {
   production: false,
   apiEndpoint: "https://localhost:2000/api/",
-  ApiKey: "openweathermap_KkrCzQHrbigJNo9WrFm4yWswF9OeeQo9co55Xg1U",
+  ApiKey: {
+    "key1": "openweathermap_cf57ce9a-b9da-454f-8c0e-6f7527ba4370",
+    "key2": "openweathermap_e04852c3-7db3-4482-986a-003b91f08a0b",
+    "key3": "openweathermap_c03c0be7-aad2-4410-8bd7-52625dec3fd5",
+    "key4": "openweathermap_65ecb40d-058b-404c-9b22-1a6546edb5dc",
+    "key5": "openweathermap_0f1224ef-3aa1-4a47-b2d3-c379a4edb01b"
+  },
+  key: "openweathermap_cf57ce9a-b9da-454f-8c0e-6f7527ba4370",
 };
